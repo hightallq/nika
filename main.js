a9d2cc4337f3671ce1183bbf9347e9634dd1d188
Bruno <77980627+hightallq@users.noreply.github.com>
2023-10-21 11:35:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,29 @@ function removeLoader() {
         let loader = document.querySelector('.intro-sector');
         loader.classList.add("hidden");
     },
-        5000);
+        2000);
+}
+
+
+
+// Function of side bar menu
+
+let burger = document.querySelector('.burger');
+console.log('burger', burger);
+let sideNavbar = document.querySelector('.side-navbar-wrap');
+console.log('side-navbar-wrap', sideNavbar);
+
+
+function burgerActive() {
+    if (sideNavbar.classList.contains("active")) {
+        sideNavbar.classList.add("disactive");
+        sideNavbar.classList.remove("active");
+        burger.classList.add("disactive-burger");
+        burger.classList.remove("activate");
+    } else {
+        sideNavbar.classList.add("active");
+        sideNavbar.classList.remove("disactive");
+        burger.classList.add("activate");
+        burger.classList.remove("disactive-burger");
+    }
 }
